cb4d22ae8d519fa3e718ab01b36f2523674e2773
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-07 22:52:35 -0300

    Add Big Decimal

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,27 @@
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 fun main(args: Array<String>) {
-    aggregations()
+    val salary = bigDecimalArrayOf("1500.55", "2000.00", "10000.00", "800.00")
+
+    println(salary.contentToString())
+
+    val increase = "1.1".toBigDecimal()
+    val salaryWithIncrease = salary
+        .map { wage ->
+            if (wage < "5000".toBigDecimal()) {
+                wage + "500".toBigDecimal()
+            } else {
+                (wage * increase).setScale(2, RoundingMode.UP)
+            }
+        }
+        .toTypedArray()
+
+    println("Salary with increase ${salaryWithIncrease.contentToString()}")
 }
 
+fun bigDecimalArrayOf(vararg vals: String): Array<BigDecimal> {
+    return Array(vals.size) { i ->
+        vals[i].toBigDecimal()
+    }
+}
